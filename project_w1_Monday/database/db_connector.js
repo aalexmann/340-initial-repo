e87a3aab82a78ce309dc56d1a9eb13a5e4116bbb
aalexmann <55524595+aalexmann@users.noreply.github.com>
2021-03-29 18:58:37 -0700

    updated user info in db_connector

--- project_w1_Monday/database/db_connector.js
+++ project_w1_Monday/database/db_connector.js
@@ -7,9 +7,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_[your_onid]',
-    password        : '[your_db_password]',
-    database        : 'cs340_[your_onid]'
+    user            : 'cs340_valdival',
+    password        : '6715',
+    database        : 'cs340_valdival'
 })
 
 // Export it for use in our applicaiton
