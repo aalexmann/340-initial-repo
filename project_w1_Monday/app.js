c5936640c117b7084b5fe53daaf4716d678ecc81
Alex Hidalgo <55524595+aalexmann@users.noreply.github.com>
2021-04-14 16:07:03 -0700

    added var db

--- project_w1_Monday/app.js
+++ project_w1_Monday/app.js
@@ -4,6 +4,7 @@
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 1776;                 // Set a port number at the top so it's easy to change in the future
+var db = require('./database/db-connector')
 
 /*
     ROUTES
